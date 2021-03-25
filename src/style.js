780a3441779ac68e0e3dc9ee412738d11879140c
Glitch (starter-cra-and-express) <none>
2021-03-25 02:39:25 +0000

    🌃🚏 Checkpoint ./src/App.js:27587200/2419 ./.glitch-assets:27587200/362 ./style.js:27587200/300

---
+++ src/style.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    marginRight: '15px',
+  },
+}));
