a538816e584ff81166d4f84fec5e9dc5dd4c816f
Glitch (final-project-demo1) <none>
2021-03-27 21:54:30 +0000

    👾💧 Checkpoint ./routes/food.js:27587200/1039

--- routes/food.js
+++ routes/food.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 // const AuthorController = require("../controllers/food.js");
 
 const router = express.Router();
@@ -6,39 +7,19 @@ const foodapi = process.env.API_KEY;
 
 router.get("/", (req, res)=>{
   var url = 'https://api.edamam.com/api/food-database/v2/parser?ingr=red%20apple' + foodapi;
-
-  var xhr = createCORSRequest('POST', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    res.json(text)
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.setRequestHeader('Content-Type', 'application/json');
+  
+  axios.get(url)
+    .then(response => {
+      if (!response.data.parser.length) {
+        return res.send({
+          error: 'No food found'
+        })
+      }
+      res.send(JSON.stringify(response.data.parser))
+    })
+    .catch(error => res.sendStatus(error.response.status))
   
 });
-// router.post("/", AuthorController.addAuthor);
 
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
 
 module.exports = router;
