e9d46f9f9c98e54d298b80883bb4e61c11a31de9
Glitch (final-project-nnbb) <none>
2021-03-29 23:19:26 +0000

    🛬🎟 Checkpoint ./src/components/Author/authors.js:27587200/300

--- src/components/Author/authors.js
+++ src/components/Author/authors.js
@@ -4,10 +4,17 @@ import "./authors.css";
 class Authors extends Component {
   constructor() {
     super();
+    console.log("constructor");
     this.state = { authors: [] };
   }
 
+  // static getDerivedStateFromProps(){
+  //   console.log("getDerivedStateFromProps");
+  //   return {authors :[]};
+  // }
+  
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("/api/author")
       .then(res => res.json())
       .then(authors =>
@@ -18,6 +25,7 @@ class Authors extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div>
         <h2>Authors</h2>
