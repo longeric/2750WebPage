2f5a69571915b269f4c19226389562d7f9be3f5a
Glitch (final-project-nnbb) <none>
2021-04-02 19:47:47 +0000

    🚜🌯 Checkpoint ./controllers/auth.js:27587200/807 ./src/components/Auth/auth.js:27587200/1

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,25 @@
 const passport = require('passport');
 const User = require("../models/user.js");
 
-exports.login = (req, res) =>{
-  passport.authenticate("login", {
-  successRedirect: "/",
-  failureRedirect: "/login",
-  failureFlash: true
-})
-}
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const oldUser = await User.findOne({ email });
+
+    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 exports.signup = (req, res) =>{
   
--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -21,7 +21,7 @@ const Auth = () => {
 
     var user = JSON.stringify({email: e.target.email.value, password: e.target.email.password})
     console.log(e.target.email.value)
-    axios.post("/login", user).then(res => console.log(res))
+    axios.post("/login/", user).then(res => console.log(res))
     // axios.post("/login/", user);
     // if (isSignup) {
     //   dispatch(signup(form, history));
