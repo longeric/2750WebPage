618c84b079263f31b85211eb77db9ec266914762
Glitch (final-project-nnbb) <none>
2021-04-02 00:54:39 +0000

    💺🎸 Checkpoint ./src/App.js:27587200/1750 ./package.json:27587200/73 ./src/components/Home/home.js:27587200/1701 ./src/components/Auth/auth.js:27587200/3901

--- src/App.js
+++ src/App.js
@@ -1,51 +1,20 @@
 import React from "react";
-import 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 import logo from "./logo.svg";
 import "./App.css";
-import Authors from "./components/Author/authors";
-import PostForm from "./components/Form/PostForm/PostForm.js";
-import Posts from "./components/Posts/Posts.js";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import useStyles from "./style";
+import Home from './components/Home/home.js';
+import Auth from './components/Home/auth.js';
 
 const App = () => {
-  const classes = useStyles();
-
-  return (
+    <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.AppBar} position="static" color="inherit">
-        <Typography variant="h2" align="center">
-          <img
-            className={classes.image}
-            src="https://cdn.glitch.com/b32b2bd2-2e97-4726-9fc6-3c049530080e%2Fworkout.png?v=1616639766877"
-            alt="workout icon"
-            height="70"
-            width="150"
-            align="center"
-          />
-          Workout Blog
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts />
-            </Grid>
-            <Grid item xs={12} sm={7}>
-              <PostForm />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-      <Authors />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
-  );
+  </BrowserRouter>
 };
 
 export default App;
---
+++ src/components/Home/home.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import Authors from "../Author/authors";
+import PostForm from "../Form/PostForm/PostForm.js";
+import Posts from "../Posts/Posts.js";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import useStyles from "../style";
+
+export default class Home extends Component {
+  
+  
+  render() {
+    const classes = useStyles();
+    
+    return (
+      <Container maxWidth="lg">
+      <AppBar className={classes.AppBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          <img
+            className={classes.image}
+            src="https://cdn.glitch.com/b32b2bd2-2e97-4726-9fc6-3c049530080e%2Fworkout.png?v=1616639766877"
+            alt="workout icon"
+            height="70"
+            width="150"
+            align="center"
+          />
+          Workout Blog
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <PostForm />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+      <Authors />
+    </Container>
+    );
+  }
+}
