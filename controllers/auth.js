bacd5dea13fb23b0fc83daaa81528f8f730a96e5
Glitch (final-project-nnbb) <none>
2021-04-03 00:32:01 +0000

    🎐🌵 Checkpoint ./controllers/auth.js:27587200/115 ./server/server.js:27587200/3

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-import jwt from "jsonwebtoken";
+const jwt = require("jsonwebtoken");
 const User = require("../models/user.js");
 
 exports.login = async (req, res) => {
@@ -31,16 +31,16 @@ exports.signup = async (req, res) =>{
 
   
   try {
-    const user = await UserModal.findOne({ email });
+    const user = await User.findOne({ email });
 
     if (user) {
         req.flash("error", "User already exists");
-        return res.redirect("/signup");
+        // return res.redirect("/signup");
     }
 
     var newUser = new User ({ nickname, email, password });
 
-    const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
+    const token = jwt.sign( { email: newUser.email, id: newUser._id }, process.env.SECRET, { expiresIn: "1h" } );
 
     res.status(201).json({ newUser, token });
   } catch (error) {
