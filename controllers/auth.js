deca4b7e843523fc3403013b8b2f00bd75c56536
Glitch (final-project-nnbb) <none>
2021-04-03 15:50:30 +0000

    🍜🔑 Checkpoint ./controllers/auth.js:27587200/166

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,35 +3,31 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.js");
 
 exports.login = async (req, res) => {
-  // const { email, password } = req.body;
-  console.log(req.body);
+  const { email, password } = req.body;
+//   console.log(req.body);
   
-  res.send({ token: "test123" });
+//   res.send({ token: "test123" });
 
-//   try {
-//     const oldUser = await User.findOne({ email });
+  try {
+    const oldUser = await User.findOne({ email });
 
-//     if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
+    if (!oldUser) return res.status(404).json({ message: "User doesn't exist" });
 
-//     const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
 
-//     if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
-//     const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, { expiresIn: "1h" });
 
-//     res.status(200).json({ result: oldUser, token });
-//   } catch (err) {
-//     res.status(500).json({ message: "Something went wrong" });
-//   }
+    res.status(200).json({ result: oldUser, token });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 exports.signup = async (req, res) =>{
 
   const { username, email, password } = req.body;
-  console.log(req.body);
-  console.log(req.body.username);
-  console.log(email);
-  console.log(password);
   
   try {
     const user = await User.findOne({ email });
@@ -57,10 +53,10 @@ exports.signup = async (req, res) =>{
   // res.send({ token: "test456" });
 }
 
-// exports.auth = () = {
-//   passport.authenticate("login", {
-//     successRedirect: "/home",
-//     failureRedirect: "/",
-//     failureFlash: true
-//   })
-// }
+exports.auth = () = {
+  passport.authenticate("login", {
+    successRedirect: "/home",
+    failureRedirect: "/",
+    failureFlash: true
+  })
+}
