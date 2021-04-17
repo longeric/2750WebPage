b5e612cf8489fbb9c78383f40dbb83daaf1307f7
Glitch (final-project-nnbb) <none>
2021-04-17 17:09:07 +0000

    🎈👴 Checkpoint ./src/actions/auth.js:27587200/2

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -35,7 +35,7 @@ export const loadUser = () => async dispatch => {
 
 //REGISTER THE USER
 
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ( name, email, password ) => async dispatch => {
   const config = {
     headers: {
       "Content-Type": "application/json"
