618827c87f89347dc1bc924e3716deb24db7d227
Glitch (final-project-nnbb) <none>
2021-04-17 16:34:36 +0000

    🚋🎥 Checkpoint ./src/index.css:27587200/4 ./src/App.js:27587200/83 ./src/style.js:27587200/77

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Alert from "./components/Layout/Alert.js";
 import { loadUser } from "./actions/auth.js";
 import setAuthToken from "./utils/setAuthToken.js";
 import Navbar from "./components/Layout/Navbar";
-import Scheduler from "./components/Scheduler/Scheduler.js";
+// import Scheduler from "./components/Scheduler/Scheduler.js";
 
 //redux
 import { Provider } from "react-redux";
@@ -30,8 +30,7 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-         < Navbar />
-          <Route exact path="/" component={Scheduler} />
+
           <section className="container">
             <Alert />
             <Switch>
@@ -46,5 +45,3 @@ const App = () => {
 };
 
 export default App;
-
-
