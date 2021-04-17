9a89413193867c4d5fded058e98605e227ac08e7
Glitch (final-project-nnbb) <none>
2021-04-17 21:45:51 +0000

    🚜🏌 Checkpoint ./models/User.js:27587200/1

--- models/User.js
+++ models/User.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
   },
   admin: {
     type: Boolean,
-    defaulst: false
+    default: false
   },
   schedule: {type: Array},
   unschedule: {type: Array}
