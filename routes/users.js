ad9bf2914a7989c840fefd6b914bffd90311769e
Glitch (final-project-nnbb) <none>
2021-04-21 02:59:05 +0000

    💽🚥 Checkpoint ./src/actions/auth.js:27587200/41 ./routes/users.js:27587200/1877:27590119/93

--- routes/users.js
+++ routes/users.js
@@ -1,89 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
 const User = require("../models/User.js");
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 
-//route: POST api/users
-// user register
-router.post(
-  "/",
-  [
-    check("name", "Name is required")
-      .not()
-      .isEmpty(),
-    check("email", "please include a valid email").isEmail(),
-    check(
-      "password",
-      "please enter a password with 6 or more characters"
-    ).isLength({ min: 6 })
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
 
-    const { name, email, password } = req.body;
-
-    try {
-      //see if the user exists
-      let user = await User.findOne({ email });
-      if (user) {
-        res.status(400).json({ errors: [{ msg: "User already exists" }] });
-      }
-      //get users gravator
-
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm"
-      });
-
-      user = new User({
-        name,
-        email,
-        avatar,
-        password
-      });
-
-      const salt = await bcrypt.genSalt(10);
-
-      //encrypt password
-      user.password = await bcrypt.hash(password, salt);
-
-      //return promise
-      await user.save();
-
-      //return json web token
-      const payload = {
-        user: {
-          id: user.id
-        }
-      };
-
-      jwt.sign(
-        payload,
-        process.env.SECRET,
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
-
-      console.log(req.body);
-      console.log("SECRET", process.env.SECRET);
-
-      // res.send("User registered");
-    } catch (err) {
-      //server error
-      console.error(err.message);
-      res.status(500).send("Server error in api users");
-    }
-  }
-);
+router.put("/updateProfile", (req,res) =>{
+  const user = {}
+  
+})
 
 module.exports = router;
--- src/actions/auth.js
+++ src/actions/auth.js
@@ -47,7 +47,7 @@ export const register = ( {name, email, password} ) => async dispatch => {
   try {
     const res = await axios.post("/api/users", body, config);
     
-    console.log(res);
+    console.log(JSON.stringify(res));
     dispatch({
       type: LOGIN_SUCCESS,
       payload: res.data,
@@ -76,7 +76,7 @@ export const login = ( email, password ) => async dispatch => {
     }
   };
 
-  console.log(email + password)
+  // console.log(email + password)
   
   const body = JSON.stringify({ email, password });
 
@@ -85,7 +85,7 @@ export const login = ( email, password ) => async dispatch => {
   try {
     const res = await axios.post("/api/auth", body, config);
 
-    console.log("res:" + res);
+    console.log("res:" + JSON.stringify(res));
 
     dispatch({
       type: LOGIN_SUCCESS,
