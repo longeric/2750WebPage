f258a0fb9db291925ac723f217af1a80cb5bcf1d
Glitch (final-project-nnbb) <none>
2021-04-21 05:32:30 +0000

    🏥⭐️ Checkpoint ./src/components/Scheduler/Scheduler.js:27587200/16

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -60,7 +60,7 @@ export default class ViewConfigurations extends SampleBase {
   }
   onActionComplete(event) {
     console.log("complete");
-    console.log(this.state.data.data);
+    console.log(this);
   }
   onActionFailure() {
     console.log("fail");
