42428b6e230e717813a43097a22a9cd8ead431a4
Glitch (final-project-nnbb) <none>
2021-04-21 15:25:15 +0000

    🚋🏙 Checkpoint ./routes/users.js:27587200/2059 ./server/server.js:27587200/2 ./models/schedule.js:27587200/502

---
+++ models/schedule.js
@@ -0,0 +1,30 @@
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  },
+  schedule: { type: Array },
+  unschedule: { type: Array }
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
