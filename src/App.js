06ba070c965883e153452cd3fee5ef8303bd1404
Glitch (final-project-nnbb) <none>
2021-04-23 19:13:51 +0000

    👘🏫 Checkpoint ./src/App.js:27587200/75

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register}></Route>
               <Route exact path="/login" component={Login}></Route>
+              <Route exact path="/pomodora" component={Pomodora}></Route> 
             </Switch>
           </section>
         </Fragment>
