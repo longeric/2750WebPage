b1772b7a8c5681e0c5ca183dd0be4f9cf2c20b37
Glitch (final-project-nnbb) <none>
2021-04-25 21:05:37 +0000

    🏄🏨 Checkpoint ./src/components/Auth/Login.js:27587200/1338

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from "react";
 import { Link, Redirect } from "react-router-dom";
-// import { GoogleLogin } from "react-google-login";
-// import { Button } from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
+import { Button } from "@material-ui/core";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/auth.js";
@@ -28,21 +28,21 @@ const Login = ({ login, isAuthenticated }) => {
     login(email, password);
   };
 
-//   const googleSuccess = async res => {
-//     const result = res?.profileObj;
-//     const token = res?.tokenId;
+  const googleSuccess = async res => {
+    const result = res?.profileObj;
+    const token = res?.tokenId;
 
-//     //     try {
-//     //       dispatch({ type: AUTH, data: { result, token } });
+    //     try {
+    //       dispatch({ type: AUTH, data: { result, token } });
 
-//     //       history.push('/');
-//     //     } catch (error) {
-//     //       console.log(error);
-//     //     }
-//   };
+    //       history.push('/');
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+  };
 
-//   const googleError = () =>
-//     alert("Google Sign In was unsuccessful. Try again later");
+  const googleError = () =>
+    alert("Google Sign In was unsuccessful. Try again later");
 
   if (isAuthenticated) {
     return <Redirect to="/scheduler" />;
@@ -76,7 +76,23 @@ const Login = ({ login, isAuthenticated }) => {
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Login" />
-        <a class="container" className="btn btn-primary" href="/auth/github">Log In with GitHub</a>
+        <GoogleLogin
+          clientId="894835653167-icpg1mdrbp9qr6tlpiosgr3ejnr5qdav.apps.googleusercontent.com"
+          render={renderProps => (
+            <Button
+              color="primary"
+              fullWidth
+              onClick="renderProps.onClick"
+              disable={renderProps.disabled}
+              variant="contained"
+            >
+              Google Sign In
+            </Button>
+          )}
+          onSuccess={googleSuccess}
+          onFailure={googleError}
+          cookiePolicy="single_host_origin"
+        />
       </form>
       <p className="my-1">
         Don't have an account? <Link to="/register">Sign up</Link>
@@ -102,20 +118,3 @@ export default connect(
 // export default Login;
 
 
-// <GoogleLogin
-//           clientId=""
-//           render={renderProps => (
-//             <Button
-//               color="primary"
-//               fullWidth
-//               onClick="renderProps.onClick"
-//               disable={renderProps.disabled}
-//               variant="contained"
-//             >
-//               Google Sign In
-//             </Button>
-//           )}
-//           onSuccess={googleSuccess}
-//           onFailure={googleError}
-//           cookiePolicy="single_host_origin"
-//         />
