0acfb7c02420b921443ce271656cff4b59c717b9
Glitch (final-project-nnbb) <none>
2021-04-26 23:10:17 +0000

    🏵🍚 Checkpoint ./src/components/Layout/Landing.js:27587200/96:27593621/4594 ./src/App.css:27587200/6:27593621/26

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -7,7 +7,14 @@ const Landing = () => {
   return (
     <Jumbotron>
       <section className="landing">
-        <div className="dark-overlay">
+        <div
+          className="dark-overlay"
+          style={{
+            backgroundImage: `url("https://cdn.glitch.com/b32b2bd2-2e97-4726-9fc6-3c049530080e%2Fa00556a1-611a-45b2-a684-f6ec0286ab2a.image.png?v=1619477524531")`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+          }}
+        >
           <div className="landing-inner">
             <h1 className="x-large">Welcome to our todo list</h1>
             <p className="lead">Help manage your time!</p>
